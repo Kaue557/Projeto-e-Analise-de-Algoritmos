a3eed0a6982c670981cb31ae3945804c97c03716
Kauê Lima <kauelima557@gmail.com>
2025-10-21 01:31:29 +0000

    tarefa maxHeap quase final

--- Max Heap/cria-heap-max.c
+++ Max Heap/cria-heap-max.c
@@ -6,8 +6,16 @@ Seu programa deve imprimir o max-heap final.
 #include <stdio.h>
 #include <stdbool.h>
 
-void afunda(int vetor, int i, int  n){ // void?
-    int i = 1; // comecando em 1 para facilitar o manejo
+
+void afunda(int vetor[], int i, int  n){ // void?
+    int aux = 0; // var auxiliar p/ troca
+
+    if(vetor[i] < vetor[2*i]){
+        aux = vetor[i];
+        vetor[i] = vetor[2*i];
+        vetor[2*i] = aux;
+
+    }
 
     int posPai = i/2;
     int posFilhoEsq = 2*i;
@@ -16,17 +24,23 @@ void afunda(int vetor, int i, int  n){ // void?
 
 }
 
-void criaHeapMax(int vetor, int n){ // void?
-
-    for(int i = 1; i < n; i++){ // 'n'? ou sizeof blablabla?
-
+void criaHeapMax(int vetor[], int n){
+    for (int i = n/2; i >= 1; i--){
+        afunda(vetor, i, n);
     }
-
 }
 
 int main(){
-    int vetor = {0, 12, 11, 7, 15, 21, 22, 13, 9, 1, 3, 8, 3}; //ignora o indice 0 para manter a logica das contas
+    int vetor[] = {0, 12, 11, 7, 15, 21, 22, 13, 9, 1, 3, 8, 3}; //ignora o indice 0 para manter a logica das contas
     int tamanho = sizeof(vetor) / sizeof(int); // tamanho real do vetor
+
+    // impressao do vetor ordenado 
+    printf("Vetor Max Heap: \n[ ");
+    for(int j = 1; j < tamanho; j++){
+        printf("%d", vetor[j]);
+    }
+    printf(" ]");
+    
 
     return 0;
 }
