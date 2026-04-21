920d4058667bdee29068c221d4cd200d461cd540
Kauê Lima <kauelima557@gmail.com>
2026-04-21 16:54:23 +0000

    salto do sapo incio

---
+++ 4° semestre/gulosos/sapo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+n -> num. de pedras
+p -> pedra e posicao (p1, p2, ..., pn)
+delta -> diferenca permitida
+*/
+int salto_sapo(int n, int p[], float delta){
+    int u[] = {p[0]};
+    int ultima_pos = {p[0]};
+
+}
+
+
+int main(){
+
+
+
+    return 0;
+}
