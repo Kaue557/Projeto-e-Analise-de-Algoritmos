9db1308f5b9827bd407b0897beab2299c434b5b0
Kauê Lima <kauelima557@gmail.com>
2026-04-21 21:04:43 +0000

    salto do sapo

--- 4° semestre/gulosos/sapo.c
+++ 4° semestre/gulosos/sapo.c
@@ -18,26 +18,42 @@ n -> num. de pedras
 p -> pedra e posicao (p1, p2, ..., pn)
 delta -> diferenca permitida
 */
-int salto_sapo(int n, int p[], float delta, int u[], int tamU){
-    int u[MAX] = {p[0]};
-    int ultima_pos = p[0];
-    int k = 0;
+int salto_sapo(int n, int p[], int delta, int u[]){
+    int local_pedra = p[0];
+    int k = 1;
+    u[0] = p[0];
 
     for(int i = 1; i < n; i++){
-        if(p[i] - ultima_pos > delta){ // se a diferenca for maior
-            ultima_pos = p[i-1];
-            u[k] = p[k-1];
+        if(p[i] - local_pedra > delta){ // se a diferenca for maior
+            local_pedra = p[i-1];
+            u[k] = p[i-1];
             k++;
         }
     }
+    if(u[k-1] != p[n-1]){
+        u[k] = p[n-1];
+        k++;
+    }
+    
     return k;
-
 }
 
 
 int main(){
+    int n = 5;
+    int p[] = {0, 2, 4, 5, 6, 7};
+    int u[MAX];
+    int delta = 2;
 
+    int k = salto_sapo(n, p, delta, u);
 
+    printf("Pedras escolhidas:\n");
+    for(int i = 0; i < k; i++){
+        printf("%d ", u[i]);
+    }
+    printf("\n");
+
+    printf("Quantidade de saltos: %d\n", k);
 
     return 0;
 }
